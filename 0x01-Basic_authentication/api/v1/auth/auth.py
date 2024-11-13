cdec9085efd46999c343c34d9b6f64a3fc0a55ff
MarvelousMarvin7 <marvinagyei3@gmail.com>
2024-11-13 01:38:25 +0000

    basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" Class for authorization
+"""
+
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth():
+    """Handle user authentication and
+    authorization
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """return false- path and
+        excluded path
+        """
+        return False
+    
+    def authorization_header(self, request=None) -> str:
+        """ returns None - request will be the
+          Flask request object
+        """
+        return None
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ returns None - request will be the
+          Flask request object
+        """
+        return None
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> str:
       - Unauthorized status
     """
     abort(401, description="Unauthorized")
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - forbidden status
+    """
+    abort(403, description="Forbidden")
